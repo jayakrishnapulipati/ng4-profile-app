7d35ec82e43d31f3182beb1ab277d13d8898b523
Jaya Krishna <jaya.pulipati@gmail.com>
2017-06-30 18:09:34 +0530

    removed un used dependencies.

--- profile-app/src/app/app.routing.ts
+++ profile-app/src/app/app.routing.ts
@@ -1,8 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import {LoginComponent} from "./login/login.component";
-import {ProfileComponent} from "./main/profile/profile/profile.component";
-import {MainComponent} from "./main/main.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
